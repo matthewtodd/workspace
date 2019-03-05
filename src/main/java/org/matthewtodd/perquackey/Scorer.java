0d20d93e6f49709e77c6eb0b64127b4d72c54e7e
Matthew Todd <matthew@matthewtodd.org>
2019-03-05 10:16:04 -0500

    Tighten up access.

--- src/main/java/org/matthewtodd/perquackey/Scorer.java
+++ src/main/java/org/matthewtodd/perquackey/Scorer.java
@@ -2,16 +2,16 @@ package org.matthewtodd.perquackey;
 
 import java.util.Arrays;
 
-public class Scorer {
-  static final int[] COLUMN = {0, 0, 0, 50, 100, 150, 200, 350, 500, 500, 0};
-  static final int[] WORDS = {0, 0, 0, 10, 20, 50, 100, 150, 250, 500, 1500};
-  static final int[] BONUSES = {0, 0, 0, 300, 500, 800, 1200, 1850, 0, 0, 0};
+class Scorer {
+  private static final int[] COLUMN = {0, 0, 0, 50, 100, 150, 200, 350, 500, 500, 0};
+  private static final int[] WORDS = {0, 0, 0, 10, 20, 50, 100, 150, 250, 500, 1500};
+  private static final int[] BONUSES = {0, 0, 0, 300, 500, 800, 1200, 1850, 0, 0, 0};
 
-  public int score(String... words) {
+  int score(String... words) {
     return score(Arrays.asList(words));
   }
 
-  public int score(Iterable<String> words) {
+  int score(Iterable<String> words) {
     int[] counts = new int[12]; // one extra to avoid a bounds check in the bonus calculation.
     for (String word : words) {
       counts[word.length()]++;
