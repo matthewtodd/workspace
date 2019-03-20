b0057450b6f902676adb654a915d7c51f2bafb68
Matthew Todd <matthew@matthewtodd.org>
2019-03-20 15:15:16 -0400

    Maybe naming helps...
    Though I suspect some object for the possible rolls is more where we're
    heading...

--- src/main/java/org/matthewtodd/perquackey/Dice.java
+++ src/main/java/org/matthewtodd/perquackey/Dice.java
@@ -33,61 +33,37 @@ class Dice {
       Die.of("qssvwy")
   ));
 
-  private final Collection<BitSet> scenarios;
+  private final Collection<BitSet> possibleRolls;
   private final Processor<String, String> state = Flow.pipe("");
   private Letters letters = new Letters();
 
   Dice() {
-    scenarios = new ArrayList<>();
-    scenarios.add(baseScenario());
+    possibleRolls = new ArrayList<>();
+    possibleRolls.add(noDiceSpokenFor());
   }
 
   void observe(Iterable<String> words) {
     letters = StreamSupport.stream(words.spliterator(), true)
         .map(word -> word.chars().collect(Letters::new, Letters::add, Letters::addAll))
         .reduce(new Letters(), Letters::union);
-    scenarios.clear();
-    // TODO pass a Letters object
-    scenarios.addAll(calculateScenarios(letters, singletonList(baseScenario())));
+    possibleRolls.clear();
+    possibleRolls.addAll(letters.possibleRollsFrom(singletonList(noDiceSpokenFor())));
     state.onNext(letters.toString());
   }
 
   boolean couldSpell(String word) {
     // TODO could short circuit. It's unnecessary here to find all the scenarios, just one.
-    return !calculateScenarios(letters.newLettersIn(word), scenarios).isEmpty();
+    return !letters.newLettersIn(word).possibleRollsFrom(possibleRolls).isEmpty();
   }
 
   Publisher<String> state() {
     return state;
   }
 
-  private static BitSet baseScenario() {
+  private static BitSet noDiceSpokenFor() {
     BitSet scenario = new BitSet(10); // TODO increase to 13 for vulnerable turns
     scenario.set(0, 10);
     return scenario;
-  }
-
-  private static Collection<BitSet> calculateScenarios(Letters letters,
-      Collection<BitSet> scenarios) {
-    if (letters.toString().isEmpty()) {
-      return scenarios;
-    }
-
-    char letter = letters.toString().charAt(0);
-    Collection<BitSet> layer = new ArrayList<>();
-
-    for (BitSet scenario : scenarios) {
-      for (int i = scenario.nextSetBit(0); 0 <= i && i < scenario.length();
-          i = scenario.nextSetBit(i + 1)) {
-        if (dice.get(i).couldProvide(letter)) {
-          BitSet validScenario = (BitSet) scenario.clone();
-          validScenario.clear(i);
-          layer.add(validScenario);
-        }
-      }
-    }
-
-    return calculateScenarios(letters.tail(), layer);
   }
 
   private static class Die {
@@ -120,6 +96,27 @@ class Dice {
 
     private Letters(byte[] buckets) {
       this.buckets = buckets;
+    }
+
+    Collection<BitSet> possibleRollsFrom(Collection<BitSet> possibleRolls) {
+      if (isEmpty()) {
+        return possibleRolls;
+      }
+
+      char letter = head();
+      Collection<BitSet> layer = new ArrayList<>();
+
+      for (BitSet roll : possibleRolls) {
+        for (int i = roll.nextSetBit(0); 0 <= i && i < roll.length(); i = roll.nextSetBit(i + 1)) {
+          if (dice.get(i).couldProvide(letter)) {
+            BitSet validRoll = (BitSet) roll.clone();
+            validRoll.clear(i);
+            layer.add(validRoll);
+          }
+        }
+      }
+
+      return tail().possibleRollsFrom(layer);
     }
 
     private Letters copy() {
@@ -168,6 +165,10 @@ class Dice {
       }
     }
 
+    boolean isEmpty() {
+      return !iterator().hasNext();
+    }
+
     Letters union(Letters other) {
       Letters result = copy();
       Letters scratchpad = copy();
@@ -193,7 +194,7 @@ class Dice {
       return result;
     }
 
-    int head() {
+    char head() {
       Iterator<Character> iterator = iterator();
       if (iterator.hasNext()) {
         return iterator.next();
