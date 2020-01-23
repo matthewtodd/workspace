22f0bb25ebe7bfcb30f4d13b707c2a45147387c0
Matthew Todd <matthew@matthewtodd.org>
2020-01-23 15:03:40 -0500

    Generate wake-format test json!

--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -1,8 +1,20 @@
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class ExampleTest {
-  @Test fun passing() {
+  @Test fun successful() {
     assertTrue(true)
+  }
+
+  @Test fun failed() {
+    assertTrue(false)
+  }
+
+  @Test fun error() {
+    throw Exception("Boom!")
+  }
+
+  @Test @Ignore("Reason.") fun skipped() {
   }
 }
