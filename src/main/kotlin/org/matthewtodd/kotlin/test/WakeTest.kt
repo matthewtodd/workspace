36e418b642e7239a1852cf6546af9c123a80f225
Matthew Todd <matthew@matthewtodd.org>
2020-08-21 14:56:30 -0400

    More playing with tests. So fun! 🍕

---
+++ src/main/kotlin/org/matthewtodd/kotlin/test/WakeTest.kt
@@ -0,0 +1,23 @@
+import kotlin.test.FrameworkAdapter
+
+// https://mochajs.org/#third-party-reporters
+// https://github.com/mochajs/mocha-examples/blob/master/packages/third-party-reporter/lib/my-reporter.js
+class WakeTest : FrameworkAdapter {
+  // I think we're going to make a data class that gets serialized to JSON?
+  override fun suite(name: String, ignored: Boolean, suiteFn: () -> Unit) : Unit {
+    println(name)
+    suiteFn()
+  }
+
+  override fun test(name: String, ignored: Boolean, testFn: () -> Any?) : Unit {
+    // TODO handle ignored
+    try {
+      testFn()
+      println(name)
+    } catch (e: AssertionError) {
+      println(e.message)
+    } catch (t: Throwable) {
+      println(t.message)
+    }
+  }
+}
--- src/test/kotlin/ExampleTest.kt
+++ src/test/kotlin/ExampleTest.kt
@@ -1,8 +1,16 @@
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class ExampleTest {
+  @BeforeTest fun setup() {
+  }
+
+  @AfterTest fun teardown() {
+  }
+
   @Test fun successful() {
     assertTrue(true)
   }
