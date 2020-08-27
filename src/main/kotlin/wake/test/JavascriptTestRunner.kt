035eff74ecf427a52389542d765c12af0d1a888b
Matthew Todd <matthew@matthewtodd.org>
2020-08-27 12:27:22 -0400

    Collapse common root package org.matthewtodd. 🍩
    https://kotlinlang.org/docs/reference/coding-conventions.html#source-code-organization

---
+++ src/main/kotlin/wake/test/JavascriptTestRunner.kt
@@ -0,0 +1,25 @@
+package org.matthewtodd.wake.test
+
+import kotlin.test.FrameworkAdapter
+
+// https://mochajs.org/#third-party-reporters
+// https://github.com/mochajs/mocha-examples/blob/master/packages/third-party-reporter/lib/my-reporter.js
+class WakeTest : FrameworkAdapter {
+    // I think we're going to make a data class that gets serialized to JSON?
+    override fun suite(name: String, ignored: Boolean, suiteFn: () -> Unit) {
+        println(name)
+        suiteFn()
+    }
+
+    override fun test(name: String, ignored: Boolean, testFn: () -> Any?) {
+        // TODO handle ignored
+        try {
+            testFn()
+            println(name)
+        } catch (e: AssertionError) {
+            println(e.message)
+        } catch (t: Throwable) {
+            println(t.message)
+        }
+    }
+}
