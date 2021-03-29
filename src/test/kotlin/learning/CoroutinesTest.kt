0e24305934fd8684c528397859cb8c9ae2d1bf3b
Matthew Todd <matthew@matthewtodd.org>
2021-03-29 10:08:13 -0400

    Kidogo kidogo... 🚀

--- src/test/kotlin/learning/CoroutinesTest.kt
+++ src/test/kotlin/learning/CoroutinesTest.kt
@@ -1,37 +1,32 @@
 package org.matthewtodd.learning
 
 import app.cash.turbine.test
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.matthewtodd.wake.test.Test
 import kotlin.time.Duration
 import kotlin.time.ExperimentalTime
 import kotlin.time.minutes
 
-// TODO compiler parameters!
-// TODO -Xopt-in=kotlin.time.ExperimentalTime
 @OptIn(ExperimentalTime::class)
 class CoroutinesTest {
-  // TODO suspend fun is automatically runBlocking by wake.test
-  // Or perhaps we want runBlockingTest,
-  // Perhaps with some affordance for working with the DelayController.
+
   @Test fun successful() = runBlocking {
-    @Suppress("UNUSED_VARIABLE")
     val timer = Timer(3.minutes)
 
-    val flow = flow {
-      emit(42)
-    }
-
-    flow.test {
+    timer.remaining().test {
       expectItem()
       expectComplete()
     }
   }
 
   class Timer(private val duration: Duration) {
-    enum class State {
-      READY, RUNNING, PAUSED, DONE
+    fun start() {}
+
+    fun remaining(): Flow<Duration> {
+      return flowOf(duration)
     }
   }
 }
