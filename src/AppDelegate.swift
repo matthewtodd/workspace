cbbb0a0c762d06cd7cd991371b2c7b30da09aef5
Matthew Todd <matthew@matthewtodd.org>
2022-10-29 14:05:21 -0400

    Attempt to log from Wren.
    It's not working yet -- event the top-level logger calls -- and I'm not
    yet sure what to do. No log lines in either the Xcode console or
    Console.app.
    
    I'll come back with fresh eyes another day.

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -1,13 +1,51 @@
+import os
 import Cocoa
 import Wren
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    private let logger: Logger = Logger()
+    private var vm: OpaquePointer?
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        logger.error("applicationDidFinishLaunching")
+
+        var config: WrenConfiguration = WrenConfiguration();
+
+        withUnsafeMutablePointer(to: &config) {
+            wrenInitConfiguration($0)
+        }
+
+        config.writeFn = { (vm: OpaquePointer?, text: UnsafePointer<CChar>?) -> Void in
+            // TODO I think there's some swifty way to say this, maybe with?
+            // TODO AppDelegate will get huge if I keep doing this. Pull out a type for the UserData?
+            let me: AppDelegate = getUserData(vm: vm)!
+            me.logger.warning("\(String(cString: text!))")
+        }
+
+        vm = withUnsafeMutablePointer(to: &config) {
+            wrenNewVM($0)
+        }
+
+        setUserData(vm: vm, userData: self)
+
+        let _ = withUnsafePointer(to: "foo") { (module) in
+            withUnsafePointer(to: "System.print(\"Whee!\")") { (source) in
+                wrenInterpret(vm, module, source)
+            }
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        logger.error("applicationWillTerminate")
+        wrenFreeVM(vm)
     }
+}
+
+private func getUserData<T: AnyObject>(vm: OpaquePointer?) -> T? {
+    return Unmanaged<T>.fromOpaque(wrenGetUserData(vm)).takeUnretainedValue()
+}
+
+private func setUserData<T: AnyObject>(vm: OpaquePointer?, userData: T) {
+    wrenSetUserData(vm, Unmanaged.passUnretained(userData).toOpaque())
 }
