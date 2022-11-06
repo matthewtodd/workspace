e5ef9244cd29aafa0464dedb8ea1e833771b0cef
Matthew Todd <matthew@matthewtodd.org>
2022-11-05 21:09:01 -0400

    Use Nest in AppDelegate!

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -1,50 +1,16 @@
-import os
 import Cocoa
-import Wren
+import os
+import src_nest
 
 public class AppDelegate: NSObject, NSApplicationDelegate {
-    private let logger: Logger = Logger()
-    private var vm: OpaquePointer?
+    private let nest: Nest
+
+    public override init() {
+        let logger = Logger()
+        self.nest = Nest(logger: { (message: String) in logger.info("\(message)") })
+    }
 
     public func applicationDidFinishLaunching(_ aNotification: Notification) {
-        logger.error("applicationDidFinishLaunching")
-
-        var config: WrenConfiguration = WrenConfiguration();
-
-        withUnsafeMutablePointer(to: &config) {
-            wrenInitConfiguration($0)
-        }
-
-        config.writeFn = { (vm: OpaquePointer?, text: UnsafePointer<CChar>?) -> Void in
-            // TODO I think there's some swifty way to say this, maybe with?
-            // TODO AppDelegate will get huge if I keep doing this. Pull out a type for the UserData?
-            let me: AppDelegate = getUserData(vm: vm)!
-            me.logger.warning("\(String(cString: text!))")
-        }
-
-        vm = withUnsafeMutablePointer(to: &config) {
-            wrenNewVM($0)
-        }
-
-        setUserData(vm: vm, userData: self)
-
-        let _ = withUnsafePointer(to: "foo".utf8CString.first!) { (module: UnsafePointer<CChar>) in
-            withUnsafePointer(to: "System.print(\"Whee!\")".utf8CString.first!) { (source: UnsafePointer<CChar>) in
-                wrenInterpret(vm, module, source)
-            }
-        }
+        nest.evaluate(code: "System.print(\"WE DID IT!\")")
     }
-
-    public func applicationWillTerminate(_ aNotification: Notification) {
-        logger.error("applicationWillTerminate")
-        wrenFreeVM(vm)
-    }
-}
-
-private func getUserData<T: AnyObject>(vm: OpaquePointer?) -> T? {
-    return Unmanaged<T>.fromOpaque(wrenGetUserData(vm)).takeUnretainedValue()
-}
-
-private func setUserData<T: AnyObject>(vm: OpaquePointer?, userData: T) {
-    wrenSetUserData(vm, Unmanaged.passUnretained(userData).toOpaque())
 }
