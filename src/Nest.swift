7a9c9c3b355b0777daebc5174c3c60c52a382499
Matthew Todd <matthew@matthewtodd.org>
2022-11-13 09:03:34 -0600

    Make some things private.
    This might be good? Separate bridging from work.

--- src/Nest.swift
+++ src/Nest.swift
@@ -19,10 +19,10 @@ public struct NestModule {
 }
 
 public class Nest {
-    // TODO make these private
-    let vm: OpaquePointer
-    var config = WrenConfiguration()
-    let logger: NestLogger
+    private let vm: OpaquePointer
+    private var config = WrenConfiguration()
+    private let logger: NestLogger
+    // TODO make this private
     let modules: Dictionary<String, NestModule>
 
     public init(logger: NestLogger? = nil, modules: Dictionary<String, NestModule> = Dictionary()) {
@@ -34,8 +34,8 @@ public class Nest {
             wrenInitConfiguration($0)
         }
 
-        config.writeFn = nestWriteFn
-        config.errorFn = nestErrorFn
+        config.writeFn = nestWrite
+        config.errorFn = nestError
         config.loadModuleFn = nestLoadModuleFn
         config.bindForeignMethodFn = nestBindForeignMethodFn
         // TODO pass more config here
@@ -55,39 +55,46 @@ public class Nest {
         // TODO default module name?
         wrenInterpret(self.vm, "main", source)
     }
+
+    func write(text: String) {
+        logger.info(text)
+    }
+
+    func error(type: WrenErrorType, module: String?, line: Int32, message: String) {
+        switch type {
+        case WREN_ERROR_COMPILE:
+            logger.error("[\(module!) line \(line)] [Error] \(message)")
+            break;
+        case WREN_ERROR_STACK_TRACE:
+            logger.error("[\(module!) line \(line)] in \(message)")
+            break;
+        case WREN_ERROR_RUNTIME:
+            logger.error("[Runtime Error] \(message)")
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 func getThis(vm: OpaquePointer?) -> Nest? {
     guard let opaque = wrenGetUserData(vm) else {
         return nil
     }
+
     return Unmanaged<Nest>.fromOpaque(opaque).takeUnretainedValue()
 }
 
-func nestWriteFn(vm: OpaquePointer?, text: UnsafePointer<CChar>?) {
-    if let this = getThis(vm: vm) {
-        this.logger.info("\(text)")
-    }
+func nestWrite(vm: OpaquePointer?, text: UnsafePointer<CChar>?) {
+    guard let this = getThis(vm: vm) else { return }
+    guard let unwrappedText = text else { return }
+    this.write(text: String(cString: unwrappedText))
 }
 
-func nestErrorFn(vm: OpaquePointer?, type: WrenErrorType, module: UnsafePointer<CChar>?, line: Int32, message: UnsafePointer<CChar>?) -> Void {
-    guard let this = getThis(vm: vm) else {
-        return
-    }
-
-    switch type {
-    case WREN_ERROR_COMPILE:
-        this.logger.error("[\(module) line \(line)] [Error] \(message)")
-        break;
-    case WREN_ERROR_STACK_TRACE:
-        this.logger.error("[\(module) line \(line)] in \(message)")
-        break;
-    case WREN_ERROR_RUNTIME:
-        this.logger.error("[Runtime Error] \(message)")
-        break;
-    default:
-        break;
-    }
+func nestError(vm: OpaquePointer?, type: WrenErrorType, module: UnsafePointer<CChar>?, line: Int32, message: UnsafePointer<CChar>?) -> Void {
+    guard let this = getThis(vm: vm) else { return }
+    guard let unwrappedMessage = message else { return }
+    this.error(type: type, module: module.map { String(cString: $0) }, line: line, message: String(cString: unwrappedMessage))
 }
 
 func nestLoadModuleFn(vm: OpaquePointer?, name: UnsafePointer<CChar>?) -> WrenLoadModuleResult {
@@ -121,19 +128,6 @@ func nestBindForeignMethodFn(vm: OpaquePointer?, module: UnsafePointer<CChar>?, 
     return { (vm: OpaquePointer?) in
         "bar".utf8CString.withUnsafeBytes {
             wrenSetSlotString(vm!, 0, $0.baseAddress)
-        }
-    }
-}
-
-/// This StringInterpolation extension hides a lot of the line noise of logging
-/// the C strings we get back from the Wren API. By marking it private, we
-/// ensure that it will only affect this file.
-///
-/// SeeAlso: [Super-powered string interpolation in Swift 5.0](https://www.hackingwithswift.com/articles/178/super-powered-string-interpolation-in-swift-5-0)
-private extension String.StringInterpolation {
-    mutating func appendInterpolation(_ value: UnsafePointer<CChar>?) {
-        if let unwrapped = value {
-            appendLiteral(String(cString: unwrapped))
         }
     }
 }
