e5fd9f3ca062389f02b352521bb0760e3f7c897b
Matthew Todd <matthew@matthewtodd.org>
2022-11-13 13:16:09 -0600

    Make modules private, too.

--- src/Nest.swift
+++ src/Nest.swift
@@ -18,12 +18,12 @@ public struct NestModule {
     }
 }
 
+// TODO I think config and VM are going to separate here?
 public class Nest {
     private let vm: OpaquePointer
     private var config = WrenConfiguration()
     private let logger: NestLogger
-    // TODO make this private
-    let modules: Dictionary<String, NestModule>
+    private let modules: Dictionary<String, NestModule>
 
     public init(logger: NestLogger? = nil, modules: Dictionary<String, NestModule> = Dictionary()) {
         self.logger = logger ?? NullLogger()
@@ -53,7 +53,7 @@ public class Nest {
 
     public func evaluate(source: String) {
         // TODO default module name?
-        wrenInterpret(self.vm, "main", source)
+        wrenInterpret(vm, "main", source)
     }
 
     func write(text: String) {
@@ -75,13 +75,31 @@ public class Nest {
             break;
         }
     }
+
+    func loadModule(name: String) -> WrenLoadModuleResult {
+        guard let module = modules[name] else { return WrenLoadModuleResult() }
+
+        var source: UnsafePointer<CChar>?
+        let onComplete: WrenLoadModuleCompleteFn? = Optional.none
+        let userData: UnsafeMutableRawPointer? = Optional.none
+
+        module.source.utf8CString.withUnsafeBytes {
+            // TODO free this memory - pass length to userData, freeing callback to onComplete?
+            let copy = UnsafeMutableRawBufferPointer.allocate(byteCount: $0.count, alignment: MemoryLayout<CChar>.alignment)
+            copy.copyMemory(from: $0)
+            source = UnsafeBufferPointer(copy.bindMemory(to: CChar.self)).baseAddress
+        }
+
+        return WrenLoadModuleResult(
+            source: source,
+            onComplete: onComplete,
+            userData: userData
+        )
+    }
 }
 
 func getThis(vm: OpaquePointer?) -> Nest? {
-    guard let opaque = wrenGetUserData(vm) else {
-        return nil
-    }
-
+    guard let opaque = wrenGetUserData(vm) else { return nil }
     return Unmanaged<Nest>.fromOpaque(opaque).takeUnretainedValue()
 }
 
@@ -98,28 +116,9 @@ func nestError(vm: OpaquePointer?, type: WrenErrorType, module: UnsafePointer<CC
 }
 
 func nestLoadModuleFn(vm: OpaquePointer?, name: UnsafePointer<CChar>?) -> WrenLoadModuleResult {
-    var result = WrenLoadModuleResult()
-
-    guard let this = getThis(vm: vm) else {
-        return result
-    }
-
-    guard let unwrappedName = name else {
-        return result
-    }
-
-    guard let module = this.modules[String(cString: unwrappedName)] else {
-        return result
-    }
-
-    module.source.utf8CString.withUnsafeBytes {
-        // TODO free this memory - pass length to userData, freeing callback to onComplete?
-        let copy = UnsafeMutableRawBufferPointer.allocate(byteCount: $0.count, alignment: MemoryLayout<CChar>.alignment)
-        copy.copyMemory(from: $0)
-        result.source = UnsafeBufferPointer(copy.bindMemory(to: CChar.self)).baseAddress
-    }
-
-    return result
+    guard let this = getThis(vm: vm) else { return WrenLoadModuleResult() }
+    guard let unwrappedName = name else { return WrenLoadModuleResult() }
+    return this.loadModule(name: String(cString: unwrappedName))
 }
 
 func nestBindForeignMethodFn(vm: OpaquePointer?, module: UnsafePointer<CChar>?, className: UnsafePointer<CChar>?, isStatic: Bool, signature: UnsafePointer<CChar>?) -> WrenForeignMethodFn? {
